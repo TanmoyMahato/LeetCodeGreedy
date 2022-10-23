afc2ea7fdaffb455dddc46936081f52f4cbf4329
Tanmoy Mahato <tanmoy.mato@gmail.com>
2022-10-23 08:01:34 +0530

    Minimum Sum of Absolute Differences of Pairs

---
+++ MinimumSumofAbsoluteDifferencesofPairs.java
@@ -0,0 +1,13 @@
+class Solution { 
+    long findMinSum(int[] A,int[] B,int N) { 
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        long sum=0;
+        for(int i=0; i<N; i++){
+            sum += Math.abs(A[i]-B[i]);
+        }
+        
+        return sum;
+    }
+}
